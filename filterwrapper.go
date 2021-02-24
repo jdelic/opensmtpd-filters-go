1c208e9a23d8bc8d9ad29ad739b70b9ac517cf7c
Jonas Maurus <jonas-github@maurus.net>
2021-02-24 22:54:18 +0100

    LinkIdentify not LinkIdentity

--- filterwrapper.go
+++ filterwrapper.go
@@ -46,10 +46,10 @@ func (fwi *FilterWrapperImpl) GetCapabilities() FilterDispatchMap {
 				fw.GetFilter().(LinkGreetingReceiver).LinkGreeting(fw, ev)
 			}
 	}
-	if _, ok := fwi.Filter.(LinkIdentityReceiver); ok {
+	if _, ok := fwi.Filter.(LinkIdentifyReceiver); ok {
 		reportReceivers["link-identify"] =
 			func(fw FilterWrapper, ev FilterEvent) {
-				fw.GetFilter().(LinkIdentityReceiver).LinkIdentity(fw, ev)
+				fw.GetFilter().(LinkIdentifyReceiver).LinkIdentify(fw, ev)
 			}
 	}
 	if _, ok := fwi.Filter.(LinkTLSReceiver); ok {
